4764f3a612ce5bbe6555af072dd57f1a987de28d
Matheus H <matheus.hsilvaa18@gmail.com>
2024-03-04 21:17:55 -0300

    init project

---
+++ src/main/java/br/com/rockerslab/familycashcard/FamilycashcardApplication.java
@@ -0,0 +1,13 @@
+package br.com.rockerslab.familycashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilycashcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilycashcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rockerslab/familycashcard/FamilycashcardApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rockerslab.familycashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilycashcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
