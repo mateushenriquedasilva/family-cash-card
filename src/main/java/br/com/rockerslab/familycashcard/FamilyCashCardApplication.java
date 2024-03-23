0c09f52cd0f7169342fee28bff17ebb7c031d0cd
Matheus H <matheus.hsilvaa18@gmail.com>
2024-03-22 21:58:45 -0300

    feat: create first endpoint.

--- src/main/java/br/com/rockerslab/familycashcard/FamilyCashCardApplication.java
+++ src/main/java/br/com/rockerslab/familycashcard/FamilyCashCardApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FamilycashcardApplication {
+public class FamilyCashCardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FamilycashcardApplication.class, args);
+		SpringApplication.run(FamilyCashCardApplication.class, args);
 	}
 
 }
